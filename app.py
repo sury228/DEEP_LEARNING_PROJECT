531b9dcac76931c5f4db54719e9548876400abac
sury228 <cloudsuryansh9@gmail.com>
2026-06-01 17:57:13 +0530

    5th commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import LabelEncoder, StandardScaler ,OneHotEncoder
+import pickle
+
